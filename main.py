dbba07cb9c32ff20601f03c6c47cd960ad577be1
andruxa-bukake <defaultapathy@gmail.com>
2026-01-10 02:29:42 +0300

    first script

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import selectors
 
-if __name__ == '__main__':
-    print('hello world')
-else:
-    print('hello world') # new test commit
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://google.com")
+
+    search_input = page.locator('//div[@class="RNNXgb"]//textarea')
+    search_input.fill('zdarova balbesi')
+
+    search_button = page.locator('//div[@class="FPdoLc lJ9FBc"]//input[1]')
+    search_button.click()
+
+    page.wait_for_timeout(5000)
